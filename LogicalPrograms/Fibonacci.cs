512860cf45237c67a1e03d13d48c26cfe1d80fc1
AnshumanMunmun <imanshuman47@gmail.com>
2022-08-29 15:26:39 +0530

    [Anshuman] Added: Fibonacci Series

---
+++ LogicalPrograms/Fibonacci.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class Fibonacci
+    {
+        public void Series()
+        {
+            int a = 0, b = 1, c, i, num;
+            Console.Write("Enter the number of Series: ");
+            num = int.Parse(Console.ReadLine());
+            Console.Write(a + " " + b + " "); //printing 0 and 1    
+            for (i = 2; i < num; ++i) //loop starts from 2 because 0 and 1 are already printed    
+            {
+                c = a + b;
+                Console.Write(c + " ");
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Logical Programs");
+            Console.WriteLine("Please Enter the Number for selecting the program:");
+            Console.WriteLine("1. Fibonacci Series");
+            Console.WriteLine("2. Perfect Series");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("1. Fibonicci Series");
+                    Fibonacci fibonacci = new Fibonacci();
+                    fibonacci.Series();
+                    break;
+                case 2:
+                    Console.WriteLine("2. Perfect Series");
+                    Perfect perfect = new Perfect();
+                    perfect.Series();
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
