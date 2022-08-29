abbbb72133bf5b2ae5e55134855a46c5baa05a64
AnshumanMunmun <imanshuman47@gmail.com>
2022-08-29 15:30:45 +0530

    [Anshuman] Added: Perfect Number Series

---
+++ LogicalPrograms/Perfect.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class Perfect
+    {
+        int a, b = 0, i, num;
+        public void Series()
+        {
+            Console.Write("Enter the number of Series: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= num / 2; ++i)    
+            {
+                b = num % i;
+                if (b == 0)
+                {
+                    a = a + i;
+                }
+            }
+            if (a == num)
+            {
+                Console.WriteLine(num + " is Perfect Number");
+            }
+            else
+            {
+                Console.WriteLine(num + " is not a Pefect Number ");
+            }
+            }
+        }
+    }
+}
